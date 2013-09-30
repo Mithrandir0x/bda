5921c36ff0e455fc84a84b7122bbd9f6688e4870
nixonik <nixeruser@gmail.com>
2013-09-30 21:15:19 +0200

    Update Usuario.java

--- ubticket/src/edu/ub/bda/ubticket/beans/Usuario.java
+++ ubticket/src/edu/ub/bda/ubticket/beans/Usuario.java
@@ -1,12 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.ub.bda.ubticket.beans;
 
 import java.sql.Timestamp;
 
 /**
+ * Esta clase define l'usuario
  *
  * @author domenicocitera
  */
