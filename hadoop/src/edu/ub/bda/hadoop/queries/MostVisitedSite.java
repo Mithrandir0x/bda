4249999b5f6da7a72eb34ba3a3e1d023850c429a
Mithrandir0x <mithrandir0x@gmail.com>
2013-12-15 14:00:15 +0100

    Se ha añadido ejemplo de MapReducer

--- hadoop/src/edu/ub/bda/hadoop/queries/MostVisitedSite.java
+++ hadoop/src/edu/ub/bda/hadoop/queries/MostVisitedSite.java
@@ -1,13 +1,9 @@
 package edu.ub.bda.hadoop.queries;
 
-import java.io.IOException;
-import java.util.Iterator;
-import org.apache.hadoop.mapred.JobConf;
-import org.apache.hadoop.mapred.MapReduceBase;
-import org.apache.hadoop.mapred.Mapper;
-import org.apache.hadoop.mapred.OutputCollector;
-import org.apache.hadoop.mapred.Reducer;
-import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hcatalog.mapreduce.HCatInputFormat;
 
 /**
  *
@@ -16,32 +12,13 @@ import org.apache.hadoop.mapred.Reporter;
 public class MostVisitedSite
 {
     
-    private static class Map extends MapReduceBase implements Mapper
-    {
-
-        @Override
-        public void map(Object k1, Object v1, OutputCollector outputCollector, Reporter reporter) throws IOException
-        {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-        
-    }
-    
-    private static class Reduce extends MapReduceBase implements Reducer
-    {
-
-        @Override
-        public void reduce(Object k2, Iterator itrtr, OutputCollector oc, Reporter rprtr) throws IOException
-        {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-        
-    }
-    
     public static void main(String[] args) throws Exception
     {
-        JobConf jobConfiguration = new JobConf(MostVisitedSite.class);
-        jobConfiguration.setJobName("most-visited-site");
+        Configuration conf = new Configuration();
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        
+        Job job = new Job(conf);
+        job.setInputFormatClass(HCatInputFormat.class);
         
         // Set OutputKeyClass
         // Set OutputValueClass
