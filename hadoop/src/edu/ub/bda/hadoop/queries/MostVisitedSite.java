94fb5f5b5312fe44dcee7dd8cc7d33b41b353f10
Oriol López Sánchez <Mithrandir0x@users.noreply.github.com>
2013-12-19 20:37:28 +0100

    Se han añadido comentarios de código
    También se ha dejado una implementación funcional de la consulta MostVisitedSite.

--- hadoop/src/edu/ub/bda/hadoop/queries/MostVisitedSite.java
+++ hadoop/src/edu/ub/bda/hadoop/queries/MostVisitedSite.java
@@ -1,214 +1,208 @@
 package edu.ub.bda.hadoop.queries;
 
-import edu.ub.bda.hadoop.queries.utils.ContextRequests;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 /**
- * A MapReduce query that fetches the most visited site of a day.
- * 
- * @author olopezsa13
+ * A query to find the most visited site of a day.
+ *
+ * @author domenicocitera
  */
 public class MostVisitedSite extends Configured implements Tool
 {
 
     private static final String root = "/user/hive/warehouse";
-    private static final String outRootPath = "/user/cloudera/";
+    private static final String dbName = "dcitera_olopez.db/";
+    private static final String tableName = "bda_wikidump_dcitera_olopez";
+    private static final String outRootPath = "/user/olopez/";
+    private static final boolean dev = false;
     
-    public static class Map extends Mapper<LongWritable, Text, Text, ContextRequests>
-    {
+    /**
+     * The mapper class that maps a context to the number of requests done at a specific hour.
+     *
+     */
+    public static class MyMap extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+        Text word = new Text();
         
-        private static Log log = LogFactory.getLog(Map.class);
-        
-        private static Pattern inputPattern = Pattern.compile("(.+)(\\s)(.+)(\\s)(\\d+)(\\s)(\\d+)");
-        
+        private static final Pattern inputPattern = Pattern.compile("(.+)(\\s)(.+)(\\s)(\\d+)(\\s)(\\d+)");
+
+        /**
+         * @param key - Input key - The line offset in the file - ignored.
+         * @param value - Input Value - This is the line itself.
+         * @param context - Provides access to the OutputCollector and Reporter.
+         * @throws IOException
+         * @throws InterruptedException
+         */
         @Override
-        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
-        {
-            String date = context.getConfiguration().get("date");
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
             Matcher inputMatch = inputPattern.matcher(value.toString());
-            
-            if ( inputMatch.matches() )
-            {
-                String rContext = inputMatch.group(3);
-                Long rRequests = Long.parseLong(inputMatch.group(5));
-                
-                //log.info("M[" + rContext + "] --> [" + "" + ", " + rRequests + "]" );
 
-                //context.write(new Text(rContext), new ContextRequests("", rRequests));
-                context.write(new Text(date), new ContextRequests(rContext, rRequests));
+            if (inputMatch.matches()) {
+
+                word.set(inputMatch.group(3));
+                int outValue = Integer.parseInt(inputMatch.group(5));
+
+                context.write(word, new IntWritable(outValue));
             }
         }
-        
-    }
-    
-    public static class Combiner extends Reducer<Text, ContextRequests, Text, ContextRequests>
-    {
-        
-        private static Log log = LogFactory.getLog(Combiner.class);
-        
-        @Override
-        protected void reduce(Text key, Iterable<ContextRequests> values, Context context) throws IOException, InterruptedException
-        {
-            String date = context.getConfiguration().get("date");
-            
-            Long sum = 0L;
-            for ( ContextRequests value : values )
-            {
-                sum += value.getRequests();
-            }
-            
-            log.info("C[" + date + "] --> [" + key.toString() + ", " + sum + "]" );
-            
-            context.write(new Text(date), new ContextRequests(key.toString(), sum));
-        }
-        
-    }
-    
-    public static class Reduce extends Reducer<Text, ContextRequests, Text, Text>
-    {
-        
-        private static Log log = LogFactory.getLog(Reduce.class);
-        
-        @Override
-        protected void reduce(Text key, Iterable<ContextRequests> values, Context context) throws IOException, InterruptedException
-        {
-            Long max = Long.MIN_VALUE;
-            String mContext = null;
-            java.util.Map<String, Long> t = new HashMap<String, Long>(); // Access to the map is slow as hell...
-            
-            for ( ContextRequests value : values )
-            {
-                //log.info("R[" + value.getContext() + "] --> [" + value.getRequests() + "]" );
-                
-                Long v;
-                if ( t.containsKey(value.getContext()) )
-                    v = t.get(value.getContext()) + value.getRequests();
-                else
-                    v = value.getRequests();
-                
-                t.put(value.getContext(), v);
-                
-                if ( v > max )
-                {
-                    max = v;
-                    mContext = value.getContext();
-                }
-            }
-            
-            log.info("R[" + key + "] --> [" + mContext + ", " + max + "]");
-            
-            context.write(new Text("[" + key + "] -->"), new Text("[" + mContext + ", " + max + "]"));
-        }
-        
-    }
-
-    
-    @Override
-    public int run(String[] args) throws Exception
-    {
-        Configuration conf = getConf();
-        args = new GenericOptionsParser(conf, args).getRemainingArgs();
-
-        // Get the input and output table names, and database name as arguments
-        String dbName = args[0] + ".db";
-        String tableName = args[1];
-        
-        String year = null, month = null, day = null;
-        year = args[2];
-        month = args[3];
-        day = args[4];
-        
-        conf.set("date", year + month + day);
-
-        Job job = new Job(conf, "most-visited-site");
-        job.setJarByClass(MostVisitedSite.class);
-        
-        Calendar calendar = Calendar.getInstance();
-        
-        FileInputFormat.setInputPaths(job, getTablePartitionPaths(year + month + day, dbName, tableName));
-        FileOutputFormat.setOutputPath(job, new Path(outRootPath + "/most-visited-site-" + year + "-" + month + "-" + day + "-" + calendar.getTimeInMillis()));
-        
-        job.setMapperClass(Map.class);
-        //job.setCombinerClass(Combiner.class);
-        job.setReducerClass(Reduce.class);
-                
-        job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(ContextRequests.class);
-
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
-        
-        return (job.waitForCompletion(true) ? 0 : 1);
     }
 
     /**
-     * For each partition of a table, get all the files inside that partition.
-     * 
-     * @param partition A partition pattern
-     * @param dbName The database name
-     * @param tableName The table name
-     * @return String A comma separated list of paths to each file inside each partition of the table
-     * @throws IOException 
+     * The reducer class that adds every request for each context, and returns the page with the
+     * maximum quantity of requests.
+     *
      */
-    private String getTablePartitionPaths(String partition, String dbName, String tableName) throws IOException
-    {
-        Pattern inputPattern = Pattern.compile("(.*)ds=" + partition + "-(\\d{4})$");
-        //Pattern inputPattern = Pattern.compile("(.*)ds=" + partition + "-0200$");
+    public static class MyRed extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+        private int max = 0;
+        private Map<String, Integer> maxcont = new HashMap<String, Integer>();
         
-        List<Path> listpath = new ArrayList<Path>();
-        FileSystem fs = FileSystem.get(new Configuration());
-        
-        FileStatus[] tableStatus = fs.listStatus(new Path(root + "/" + dbName + "/" + tableName + "/"));
-        for ( FileStatus partitionStatus : tableStatus )
+        /**
+         * @param key - Input key - Name of the context
+         * @param values - Input Value - Iterator request context
+         * @param context - Used for collecting output
+         * @throws IOException
+         * @throws InterruptedException
+         */
+        @Override
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException
         {
-            Matcher inputMatch = inputPattern.matcher(partitionStatus.getPath().toString());
-            if ( inputMatch.matches() )
+
+            int sum = 0;
+
+            for (IntWritable value : values)
             {
-                FileStatus[] rowsStatus = fs.listStatus(partitionStatus.getPath());
-                for ( FileStatus rowsFile : rowsStatus )
+                sum = sum + value.get();
+
+                if (sum >= max)
                 {
-                    System.out.println(rowsFile.getPath());
-                    listpath.add(rowsFile.getPath());
+                    max = sum;
+                    maxcont.put(key.toString(), sum);
                 }
             }
         }
-        
-        String pts = "";
-        for (Path p : listpath)
+
+        @Override
+        protected void cleanup(Context context) throws IOException, InterruptedException
         {
-            pts = pts.concat(p.toString() + ",");
+            for (String key : maxcont.keySet())
+            {
+                if (maxcont.get(key) == max)
+                {
+                    context.write(new Text(key), new IntWritable(max));
+                }
+            }
+
         }
-        
-        return pts.substring(0, (pts.length() - 1));
     }
-    
+
     public static void main(String[] args) throws Exception
     {
-        int exitCode = ToolRunner.run(new MostVisitedSite(), args);
+        MostVisitedSite driver = new MostVisitedSite();
+        int exitCode = ToolRunner.run(driver, args);
         System.exit(exitCode);
     }
-    
+
+    @Override
+    public int run(String[] args) throws Exception {
+
+        String year = null, month = null, day = null;
+
+        if (dev)
+        {
+            year = "2013";
+            month = "11";
+            day = "05";
+        }
+        else
+        {
+            if (args.length == 3)
+            {
+                year = args[0];
+                month = args[1];
+                day = args[2];
+            }
+            else
+            {
+                throw new Exception("Bad arguments");
+            }
+        }
+
+        // Create the job specification object
+        Job job = new Job(getConf());
+        job.setJarByClass(MostVisitedSite.class);
+        job.setJobName(this.getClass().getName());
+
+        // Setup input and output paths
+        String p = resPath(year + month + day);
+        FileInputFormat.setInputPaths(job, p);
+
+        Path outFilesPath = new Path(outRootPath + "/mes_vista_" + year + "-" + month + "-" + day);
+
+        // Delete and create if exist
+        FileSystem.get(new Configuration()).delete(outFilesPath, true);
+        FileOutputFormat.setOutputPath(job, outFilesPath);
+
+        // Set the Mapper and Reducer classes
+        job.setMapperClass(MyMap.class);
+        job.setReducerClass(MyRed.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        // Specify the type of output keys and values
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        boolean success = job.waitForCompletion(true);
+        return success ? 0 : 1;
+    }
+
+    /**
+     * Return a string that contains the paths to each partition of the table for a day, separated by commas.
+     * 
+     */
+    public static String resPath(String ymd) throws IOException
+    {
+        Pattern inputPattern = Pattern.compile("(.*)ds=" + ymd + "-(\\d{4})$");
+        String pts = "";
+        FileSystem fs = FileSystem.get(new Configuration());
+        FileStatus[] status = fs.listStatus(new Path(root + "/" + dbName + tableName + "/"));
+        
+        for (FileStatus statu : status)
+        {
+            Matcher inputMatch = inputPattern.matcher(statu.getPath().toString());
+            
+            if (inputMatch.matches())
+            {
+                FileStatus[] status2 = fs.listStatus(statu.getPath());
+                
+                for (FileStatus status21 : status2) {
+                    System.out.println(status21.getPath()); //test file input
+                    pts = pts.concat(status21.getPath().toString() + ",");
+                }
+            }
+        }
+        return pts.substring(0, (pts.length() - 1));
+    }
 }
